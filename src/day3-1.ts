fcbb97aa6900954519673c6fbde778813d23203b
Peter van Dijk <petervandijk@gmail.com>
2018-12-04 20:26:25 +0100

    day3-1 solved. but verrrrrrry slow +- 3minutes?

--- src/day3-1.ts
+++ src/day3-1.ts
@@ -36,7 +36,7 @@ class Claim {
 
 }
 
-function zero2D(rows, cols) {
+function null2D(rows, cols) {
   var array = [], row = [];
   while (cols--) row.push(null);
   while (rows--) array.push(row.slice());
@@ -48,23 +48,48 @@ function processInput(err: any, contents: string) {
 
   const claimArr: Claim[] = dataArr.map(str => new Claim(str));
 
-  const arrDim = 10;
-  let solution = 0;
-  const canvas: number[][] = zero2D(arrDim, arrDim);
+  const arrDim = 1500;
+  let overlaps = 0;
+  let counter = 0;
+  const canvas: number[][] = null2D(arrDim, arrDim);
+  const overlapsRegister = [
+    {
+      r: null,
+      c: null
+    }
+  ];
 
+  // const test = [];
+  // test.push({r: 1, c: 0});
+  // console.log(test);
+  // console.log(test.filter(row => row.r === 1 && row.c === 1).length === 0);
+
+  let claimCount = 0;
   claimArr.forEach(claim => {
+    claimCount++;
+    console.log(`claimCount: ${claimCount}`);
     for (let r = claim.rowStart - 1; r < claim.rowEnd; r++) {
       for (let c = claim.columnStart - 1; c < claim.columnEnd; c++) {
-        if (canvas[r][c] !== null) solution++;
+        if (canvas[r][c] !== null) {
+          if (overlapsRegister.filter(row => row.r === r && row.c === c).length === 0) {
+            overlaps++;
+            overlapsRegister.push({r, c});
+          } else {
+          
+          }
+        }
+        counter++;
+        // console.log('counter: ', counter);
         canvas[r][c] = claim.id;
       }
     }
   });
 
 
-  console.log(canvas);
+  // console.log(canvas);
   // console.log(claimArr[0]);
-  console.log(solution);
+  console.log(overlaps);
+  // console.log(overlapsRegister);
 }
 
-fs.readFile('../day3-ex.input', 'utf8', processInput);
+fs.readFile('../day3.input', 'utf8', processInput);
