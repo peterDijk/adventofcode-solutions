e5c164fe6c3d39cf4fa9d8b547c6743db70ae808
Peter van Dijk <petervandijk@gmail.com>
2018-12-04 17:20:05 +0100

    day3-1 not there yet

--- src/day3-1.ts
+++ src/day3-1.ts
@@ -8,8 +8,14 @@ class Claim {
   width: number;
   height: number;
 
-  constructor(dataStr) {
+  columnStart: number;
+  columnEnd: number;
+  rowStart: number;
+  rowEnd: number;
+
+  constructor(dataStr: string) {
     this.init(dataStr);
+    this.getArea();
   }
 
   init(dataStr) {
@@ -21,6 +27,12 @@ class Claim {
     this.height = parseInt(dataSplit[3].split("x")[1]);
   }
 
+  getArea() {
+    this.columnStart = this.fromLeft + 1;
+    this.columnEnd = this.columnStart + (this.width - 1);
+    this.rowStart = this.fromTop + 1;
+    this.rowEnd = this.rowStart + (this.height - 1);
+  }
 
 }
 
@@ -30,12 +42,19 @@ function processInput(err: any, contents: string) {
 
   const claimArr: Claim[] = dataArr.map(str => new Claim(str));
 
+  let solution = 0;
+  const canvas: number[][] = new Array(1500).fill(new Array(1500).fill(0));
+
   claimArr.forEach(claim => {
-    
-  })
+    for(let r = claim.rowStart; r !== claim.rowEnd; r++) {
+      for (let c = claim.columnStart; c !== claim.columnEnd; c++) {
+        if (canvas[r][c] !== null) solution++;
+        canvas[r][c] = claim.id;
+      }
+    }
+  });
 
-
-  console.log(claimArr);
+  console.log(solution);
 }
 
 fs.readFile('../day3.input', 'utf8', processInput);
